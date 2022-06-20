a3fcc35f2682ec19f5b7cdbc22b0ebec1c2c214e
Muhammad  Ghufran <ghufranm054@gmail.com>
2022-06-20 08:19:21 +0500

    first commit

---
+++ src/pages/HistoryDetails/HistoryDetails.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import "./user.css";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react"
+import firebase from '../../config/firebase';
+import { useState } from "react";
+import { Card } from "semantic-ui-react";
+export default function HistoryDetails() {
+  let params = useParams();
+  const db=firebase.firestore();
+  const [detimg,setImg]=useState([])
+  useEffect(()=>{
+    const items=[]
+    db.collection('guns').doc(params.historyId).collection('frames').get().then(function(querySnapshot){
+
+        querySnapshot.forEach(function(doc){
+
+            items.push(doc.data())
+        })
+        setImg(items)
+
+
+
+    })
+
+
+
+    
+
+
+},[])
+  return (
+   <div className="user">
+       <Card.Group>
+            {detimg.map((det)=>(
+                <Card color='red' image={`data:image/png;base64,${det.Image}`} />
+
+                
+                ))}
+
+
+
+            </Card.Group>
+   </div>
+  );
+}
